64b75a04c3303c686be4c7401b335d742fe8111f
kirandesai3667-del <kiran.desai3667@gmail.com>
2026-04-04 08:37:58 +0530

    OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,18 @@
+importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-messaging-compat.js');
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyC97HQL03FiX8meE2iMaVAF7EJZh7r-XAM",
+  authDomain: "batrisi-medical-sahay.firebaseapp.com",
+  projectId: "batrisi-medical-sahay",
+  storageBucket: "batrisi-medical-sahay.firebasestorage.app",
+  messagingSenderId: "632157918744",
+  appId: "1:632157918744:web:0e2df6de8a4fc274ba156d"
+});
+const messaging = firebase.messaging();
+messaging.onBackgroundMessage((payload) => {
+    const title = payload.notification?.title || payload.data?.title || 'Batrisi Notification';
+    const body = payload.notification?.body || payload.data?.body || '';
+    self.registration.showNotification(title, { body: body, icon: '/medical-report-app/icon-192.png' });
+});
