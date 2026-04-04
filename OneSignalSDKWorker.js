f761429877ee31b180aea7b53caa64223a108be0
kirandesai3667-del <kiran.desai3667@gmail.com>
2026-04-04 11:12:17 +0530

    OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,3 +1,4 @@
+// MASTER SERVICE WORKER (BATRISI APP)
 importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-messaging-compat.js');
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
@@ -6,13 +7,17 @@ firebase.initializeApp({
   apiKey: "AIzaSyC97HQL03FiX8meE2iMaVAF7EJZh7r-XAM",
   authDomain: "batrisi-medical-sahay.firebaseapp.com",
   projectId: "batrisi-medical-sahay",
-  storageBucket: "batrisi-medical-sahay.firebasestorage.app",
   messagingSenderId: "632157918744",
   appId: "1:632157918744:web:0e2df6de8a4fc274ba156d"
 });
+
 const messaging = firebase.messaging();
 messaging.onBackgroundMessage((payload) => {
-    const title = payload.notification?.title || payload.data?.title || 'Batrisi Notification';
+    const title = payload.notification?.title || payload.data?.title || 'Batrisi Update';
     const body = payload.notification?.body || payload.data?.body || '';
-    self.registration.showNotification(title, { body: body, icon: '/medical-report-app/icon-192.png' });
+    self.registration.showNotification(title, {
+        body: body,
+        icon: '/medical-report-app/icon-192.png',
+        badge: '/medical-report-app/icon-192.png'
+    });
 });
